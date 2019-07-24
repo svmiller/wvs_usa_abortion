f85e773d9cf9dd96645c9b6d6bda4c0b2262f4c3
svmiller <steven.v.miller@gmail.com>
2019-07-24 13:13:03 -0400

    change functions

--- src/2-clean.R
+++ src/2-clean.R
@@ -1,9 +1,11 @@
+# note: carr and r2sd are copies of car::recode() and arm::rescale() respectively.
+# I created these functions in my stevemisc package to avoid function clashes with those package and tidyverse packages.
 WVS_USA_Abortion %>%
-  mutate(ajd = car::recode(aj, "1=0; 2:10=1"),
-         z_age = arm::rescale(age),
-         z_ideo = arm::rescale(ideology),
-         z_satisf = arm::rescale(satisfinancial),
-         z_cai = arm::rescale(cai),
-         z_god = arm::rescale(godimportant)) -> Data  
+  mutate(ajd = carr(aj, "1=0; 2:10=1"),
+         z_age = r2sd(age),
+         z_ideo = r2sd(ideology),
+         z_satisf = r2sd(satisfinancial),
+         z_cai = r2sd(cai),
+         z_god = r2sd(godimportant)) -> Data  
 
 saveRDS(Data, "data/data.rds")
