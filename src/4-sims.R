54a9be1234a525b44300bae0ef1fbf9fcdd91fad
svmiller <steven.v.miller@gmail.com>
2019-07-24 15:24:05 -0400

    another mass update

--- src/4-sims.R
+++ src/4-sims.R
@@ -4,7 +4,7 @@ data_grid(Data,
           nesting(z_god)) %>%
   na.omit %>%
   mutate_at(vars(contains("z_"), -z_god), list(~replace(., . != 0, 0))) %>%
-  mutate(year = 0) -> newdat_zgod
+  mutate(year = 0) -> Newdat
 
 
 
@@ -13,14 +13,14 @@ data_grid(Data,
 
 
 merTools::predictInterval(Models[[1]],
-                          newdata=newdat_zgod, 
+                          newdata=Newdat, 
                           include.resid.var=F,
                           n.sims=1000, seed=8675309,
                           returnSims = TRUE) -> M1_summarysims
 
 attributes(M1_summarysims)$sim.results %>%
   data.frame() %>% tbl_df() %>%
-  bind_cols(.,newdat_zgod) %>% 
+  bind_cols(.,Newdat) %>% 
   mutate(godimportant = seq(1, 10)) %>%
   select(godimportant, everything()) %>%
   group_by(godimportant) %>%
@@ -29,7 +29,7 @@ attributes(M1_summarysims)$sim.results %>%
 
 
 merTools::predictInterval(Models[[2]],
-                          newdata=newdat_zgod, 
+                          newdata=Newdat, 
                           include.resid.var=F,
                           n.sims=1000, seed=8675309,
                           type = "probability",
@@ -38,7 +38,7 @@ merTools::predictInterval(Models[[2]],
 
 attributes(M2_summarysims)$sim.results %>%
   data.frame() %>% tbl_df() %>%
-  bind_cols(.,newdat_zgod) %>% 
+  bind_cols(.,Newdat) %>% 
   mutate(godimportant = seq(1, 10)) %>%
   select(godimportant, everything()) %>%
   mutate_at(vars(contains("X")), boot::inv.logit) %>%
